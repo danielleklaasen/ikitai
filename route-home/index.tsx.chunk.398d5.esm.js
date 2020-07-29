2b03becb3b5775768800b7a131502a0d2649d9be
Danielle Klaasen <danielleklaasen@gmail.com>
2020-07-29 12:52:11 +0200

    Updates

---
+++ route-home/index.tsx.chunk.398d5.esm.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{FA6U:function(e){e.exports={home:"home__MseGd"}},FDtd:function(e,o,n){"use strict";n.r(o);var s=n("hosL"),t=n("FA6U");o.default=()=>Object(s.h)("div",{class:t.home},Object(s.h)("h1",null,"Home"),Object(s.h)("p",null,"This is the Home component."))}}]);
